70b6a2a805d48f1165ec5e2a0d986edb49a8a4ec
allanfernds <allanfernds@gmail.com>
2022-12-03 03:58:10 -0300

    dev: install react router

---
+++ src/pages/ProfessionalForm.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class ProfessionalForm extends Component {
+  render() {
+    return (
+      <div>ProfessionalForm</div>
+    )
+  }
+}
---
+++ src/pages/SkillsForm.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class SkillsForm extends Component {
+  render() {
+    return (
+      <div>SkillsForm</div>
+    )
+  }
+}
