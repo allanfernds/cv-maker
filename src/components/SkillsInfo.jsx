d17202e5644a1c7fcecd1d8b3946d43b8041492b
allanfernds <allanfernds@gmail.com>
2022-12-03 05:42:08 -0300

    create: SkillsInfo component

---
+++ src/components/SkillsInfo.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class SkillsInfo extends Component {
+  render() {
+    return (
+      <div>
+        SkillsInfo
+        <label htmlFor='programmingLang'>
+            <p>Linguagem de programação: </p>
+            <input
+              name='programmingLang'
+              id='programmingLang'
+              type="text" 
+            />
+        </label>
+        <label htmlFor='experienceYears'>Experiencia: </label>
+          <select id='experienceYears'name="experienceYears">
+            <option>Menos de 1 ano</option>
+            <option>1 a 2 anos</option>
+            <option>2 a 3 anos</option>
+            <option>3 a 4 anos</option>
+            <option>4 a 5 anos</option>
+            <option>Mais de 5 anos</option>
+          </select>
+      </div>
+    )
+  }
+}
--- src/pages/SkillsForm.jsx
+++ src/pages/SkillsForm.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import SkillsInfo from '../components/SkillsInfo'
 
 export default class SkillsForm extends Component {
   render() {
     return (
-      <div>SkillsForm</div>
+      <div>
+        SkillsForm
+        <SkillsInfo />  
+      </div>
     )
   }
 }
