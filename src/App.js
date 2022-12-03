badf8793ab9e6f9e2318346d4cc948f7fb18b00d
allanfernds <allanfernds@gmail.com>
2022-12-03 04:04:17 -0300

    dev: configuring routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import PerssonalForm from './pages/PersonalForm'
+import { Route, Switch  } from 'react-router-dom'
+
+import PersonalForm from './pages/PersonalForm'
 import React, { Component } from 'react'
+import EducationalForm from './pages/EducationalForm'
+import ProfessionalForm from './pages/ProfessionalForm'
+import ProjectsForm from './pages/ProjectsForm'
+import SkillsForm from './pages/SkillsForm'
 
 export default class App extends Component {
   render() {
     return (
-      <>
-      <PerssonalForm />
-      </>
+      <Switch>
+        <Route exact to='/' component={ PersonalForm } />
+        <Route to='/educationalform' component={ EducationalForm } />
+        <Route to='/professionalform' component={ ProfessionalForm } />
+        <Route to='/projectsform' component={ ProjectsForm } />
+        <Route to='/skillsform' component={ SkillsForm } />
+      </Switch>
     )
   }
 }
