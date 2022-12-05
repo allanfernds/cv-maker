b949eb37fc05a16e8867de5463826d26e5880f1a
allanfernds <allanfernds@gmail.com>
2022-12-05 15:02:24 -0300

    dev: create addNewFormSection func and config

--- src/App.js
+++ src/App.js
@@ -7,10 +7,40 @@ import ProjectsInfo from './components/ProjectsInfo'
 import SkillsInfo from './components/SkillsInfo'
 
 export default class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      personalform: [{section: ''}],
+      educationallform: [{section: ''}],
+      professionalform: [{section: ''}],
+      projectsform: [{section: ''}],
+    }
+  }
+
+  addNewFormSection = () => {
+    const { personalform } = this.state
+    personalform.push({section: ''});
+
+    this.setState({
+      personalform: personalform
+    });
+  }
+
   render() {
+    const { personalform } = this.state
     return (
       <main>
-      <PersonalInfo/>
+
+        <div>
+        {personalform.map((section, index) => <PersonalInfo key={index} /> )}
+          <button onClick={this.addNewFormSection}>
+            Adicionar mais uma seção
+          </button>
+        </div>
+        <div>
+
+        </div>
       <EducationalInfo />
       <ProfessionalInfo />
       <ProjectsInfo />
