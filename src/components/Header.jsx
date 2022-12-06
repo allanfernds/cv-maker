4917262a4051aaa7010fe12e3c861fa9c913ac3c
allanfernds <allanfernds@gmail.com>
2022-12-06 17:07:41 -0300

    style: solo rename

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Header extends Component {
   render() {
     return (
-      <header><h1>CV Maker</h1></header>
+      <header><h1>CV Easy</h1></header>
     )
   }
 }
