0941d411b60306a10f7dc663cd3d6b68557fd083
allanfernds <allanfernds@gmail.com>
2022-12-06 18:59:28 -0300

    dev: create footer componente

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import '../style/header.css'
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer><p>desenvolvido por <a target='blank' href="https://www.linkedin.com/in/alanfernds/">Alan Fernandes</a></p></footer>
+    )
+  }
+}
