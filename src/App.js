4947514f99f3d15e32ec84cc7b7e86d86ab24c4d
allanfernds <allanfernds@gmail.com>
2022-12-06 16:23:32 -0300

    dev: rearranging files and create header component

--- src/App.js
+++ src/App.js
@@ -1,136 +1,21 @@
 
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import EducationalInfo from './components/EducationalInfo'
-import PersonalInfo from './components/PersonalInfo'
-import ProfessionalInfo from './components/ProfessionalInfo'
-import ProjectsInfo from './components/ProjectsInfo'
-import SkillsInfo from './components/SkillsInfo'
-import generatePDF from './pdfmake/CvPdfMaker'
+import Header from './components/Header'
+import DevCvMakerForm from './pages/DevCvMakerForm'
+
 
 class App extends Component {
-  constructor() {
-    super()
-
-    this.state = {
-      personalform: [{section: ''}],
-      educationallform: [{section: ''}],
-      professionalform: [{section: ''}],
-      projectsform: [{section: ''}],
-      skillsform: [{section: ''}],
-    }
-  }
-
-  addNewFormSection = ({target}) => {
-    console.log(target.name)
-    const { name } = target
-    this.setState(prevState => ({
-      [name]: [...prevState[name], {section: ''}]
-    }))
-    console.log(this.props)
-  }
-
-  removeFormSection = ({target}) => {
-    const { name } = target
-    const newArray = this.state[name]
-    newArray.pop()
-    newArray.length > 0 ? this.setState({ [name]: newArray}) : alert('Não é possivel remover')
-  }
 
   render() {
-    const { userInfo } = this.props
-    const { educationallform, professionalform, projectsform, skillsform } = this.state
     return (
-      <main>
-
-        <div>
-          <h3>Sobre você</h3>
-        <PersonalInfo />
-        </div>
-        
-        <div>
-        <h3>Formação</h3>
-        {educationallform.map((section, index) => <EducationalInfo key={index} /> )}
-          <div>
-          <button 
-            className='add-more-section-btn'
-            name={'educationallform'} onClick={this.addNewFormSection}>
-            +
-          </button>
-          <button 
-            className='remove-btn'
-            name={'educationallform'} onClick={this.removeFormSection}>
-            x
-          </button>
-          </div>
-          <br/>
-        </div>
-
-        <div>
-        <h3>Experiência</h3>
-        {professionalform.map((section, index) => <ProfessionalInfo key={index} /> )}
-        <div>
-          <button 
-            className='add-more-section-btn'
-            name={'professionalform'} onClick={this.addNewFormSection}>
-            +
-          </button>
-          <button 
-            className='remove-btn'
-            name={'professionalform'} onClick={this.removeFormSection}>
-            x
-          </button>
-          </div>
-          <br/>
-        </div>
-
-        <div>
-        <h3>Principais projetos</h3>
-        {projectsform.map((section, index) => <ProjectsInfo key={index} /> )}
-        <div>
-          <button 
-            className='add-more-section-btn'
-            name={'projectsform'} onClick={this.addNewFormSection}>
-            +
-          </button>
-          <button 
-            className='remove-btn'
-            name={'projectsform'} onClick={this.removeFormSection}>
-            x
-          </button>
-          </div>
-          <br/>
-        </div>
-
-        <div>
-        <h3>Hard skills</h3>
-        {skillsform.map((section, index) => <SkillsInfo key={index} /> )}
-        <div>
-          <button 
-            className='add-more-section-btn'
-            name={'skillsform'} onClick={this.addNewFormSection}>
-            +
-          </button>
-          <button 
-            className='remove-btn'
-            name={'skillsform'} onClick={this.removeFormSection}>
-            x
-          </button>
-          </div>
-          <br/>
-        </div>
-
-      <button
-      onClick={() => generatePDF(userInfo)}
-      >Gerar PDF</button>
-      </main>
+      <>
+        <Header/>
+        <DevCvMakerForm/>
+      </>
     )
   }
 }
 
 
-const mapStateToProps = (state) => ({
-    ...state
-})
-
-export default connect(mapStateToProps)(App)
+export default connect()(App)
